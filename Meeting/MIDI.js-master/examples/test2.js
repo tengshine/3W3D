7dbe8ad3a9837f41a48217d00f023a2fceea4a7e
tengshine <tengshine@gmail.com>
2018-01-12 14:46:06 +0800

    Add files via upload

--- Meeting/MIDI.js-master/examples/test2.js
+++ Meeting/MIDI.js-master/examples/test2.js
@@ -929,13 +929,13 @@ function makeEventArrayHH1 () {
   var eventArray = [];
   timeSignatureX=4;
   timeSignatureY=4;
-  eventArray.push(new Event(0   , 0.5, 72, 'D', 0.5, 0.5) );
+  eventArray.push(new Event(0   , 0.5, 72, 'D', 0.8, 0.5) );
   eventArray.push(new Event(0.5 , 0.25, 67, 'U') );
   eventArray.push(new Event(0.75, 0.25, 67, 'D') );
   eventArray.push(new Event(1   , 0.5, 66, 'U') );
   eventArray.push(new Event(1.5 , 0.5, 67, 'D') );
   
-  eventArray.push(new Event(2   , 0.5, 72, 'D', 0.5, 0.5) );
+  eventArray.push(new Event(2   , 0.5, 72, 'D', 0.8) );
   eventArray.push(new Event(2.5 , 0.25, 67, 'U') );
   eventArray.push(new Event(2.75, 0.25, 67, 'D') );
   eventArray.push(new Event(3   , 0.5, 66, 'U') );
@@ -947,7 +947,7 @@ function makeEventArrayHH1 () {
   eventArray.push(new Event(5.5 , 0.5, 67, 'U') );
   eventArray.push(new Event(6   , 0.5, 72, 'D') );
   eventArray.push(new Event(6.5 , 0.5, 67, 'U') );
-  eventArray.push(new Event(7   ,   1, 64, 'D') );
+  eventArray.push(new Event(7   , 0.5, 64, 'D') );
   eventArray.push(new Event(7.5 , 0.5, 67, 'U') );
   /*eventArray.push(new Event(0   , 0.5, 60, 'D', 0.5, 0.5) );
   eventArray.push(new Event(0.5 , 0.5, 60, 'U') );
